b185dc8af70a95563bfe81f7195512dff0e77083
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-05-12 16:41:02 +0200

    create a state that keep the barcode

--- src/components/BarcodeScaner/BarcodeInput.jsx
+++ src/components/BarcodeScaner/BarcodeInput.jsx
@@ -1,9 +1,11 @@
-function BarcodeInput({ onBarcodeWritting }) {
+function BarcodeInput({ barcode, onBarcodeWritting, handleKeyPress }) {
   return (
     <div>
       <input
         type="number"
+        value={barcode}
         onChange={(event) => onBarcodeWritting(event.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="Entrer votre Code Barre"
         className="w-64 h-8 mr-4 font-standard_font text-black placeholder:text-slate-400 bg-white 
           border border-solid border-gray-500 rounded-md text-center focus:text-black focus:bg-slate-100 focus:border-border_button focus:outline-none focus:text-center"
