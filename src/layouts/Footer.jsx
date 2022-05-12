eece7821f1bd1ba5ae8ae3e0f3c66bad84e75f63
CarolinaDelerue <delerue.carolina@gmail.com>
2022-05-12 21:27:11 +0200

    home 2

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,26 +1,26 @@
 export default function Footer() {
   return (
     <footer
-      className="text-center text-white absolute w-full bottom-0 m-auto"
+      className="text-center text-white fixed w-full bottom-0 m-auto"
       style={{ backgroundColor: "#f1f1f1" }}
     >
-        <div className="flex justify-around items-center w-full">
-          <a href="#!" className=" text-gray-800 ">
-            <img src="/src/assets/list.png" className="h-4" />
-          </a>
-          <a href="#!" className=" text-gray-800">
-            <img src="/src/assets/favorite.png" className="h-4" />
-          </a>
-          <a href="#!" className=" text-gray-800">
-            <img src="/src/assets/start.png" className="h-12" />
-          </a>
-          <a href="#!" className=" text-gray-800">
-            <img src="/src/assets/graph.png" className="h-4" />
-          </a>
-          <a href="#!" className=" text-gray-800">
-            <img src="/src/assets/cart.png" className="h-4" />
-          </a>
-        </div>
+      <div className="flex justify-around items-center w-full">
+        <a href="#!" className=" text-gray-800 ">
+          <img src="/src/assets/list.png" className="h-4" />
+        </a>
+        <a href="#!" className=" text-gray-800">
+          <img src="/src/assets/favorite.png" className="h-4" />
+        </a>
+        <a href="#!" className=" text-gray-800">
+          <img src="/src/assets/start.png" className="h-12" />
+        </a>
+        <a href="#!" className=" text-gray-800">
+          <img src="/src/assets/graph.png" className="h-4" />
+        </a>
+        <a href="#!" className=" text-gray-800">
+          <img src="/src/assets/cart.png" className="h-4" />
+        </a>
+      </div>
     </footer>
   );
 }
